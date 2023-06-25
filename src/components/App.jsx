b9f385841796c3fe02096484084952a1f2e7f6c5
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-06-25 12:02:11 +0300

    start task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Navigation from "./Navigation/Navigation";
+import { Home } from "pages/Home";
+import { Rates } from "pages/Rates";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+   <>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home/>}/>
+        <Route path='/rates' element={<Rates/>}/>
+      </Route>
+      <Route path='*' element={<Navigate to='/'/>}/>
+    </Routes>
+   </>
   );
 };
